456c99ec94cd85cde11eae2d29bc8f8e80c23207
naming <namingbe@users.noreply.github.com>
2025-06-10 16:30:33 +0200

    update tests to match python impl

--- src/test/java/org/nsvformat/TestUtils.java
+++ src/test/java/org/nsvformat/TestUtils.java
@@ -13,43 +13,43 @@ import static java.util.Map.entry;
 public class TestUtils {
     
     public static final Map<String, NsvData> SAMPLES_DATA = Map.ofEntries(
-        entry("empty", new NsvData(List.of("v:1.0"), List.of())),
-        entry("empty_one", new NsvData(List.of("v:1.0"), List.of(List.of()))),
-        entry("empty_two", new NsvData(List.of("v:1.0"), List.of(List.of(), List.of()))),
-        entry("empty_three", new NsvData(List.of("v:1.0"), List.of(List.of(), List.of(), List.of()))),
-        entry("basic", new NsvData(List.of("v:1.0"), List.of(
+        entry("empty", new NsvData(List.of(), List.of())),
+        entry("empty_one", new NsvData(List.of(), List.of(List.of()))),
+        entry("empty_two", new NsvData(List.of(), List.of(List.of(), List.of()))),
+        entry("empty_three", new NsvData(List.of(), List.of(List.of(), List.of(), List.of()))),
+        entry("basic", new NsvData(List.of(), List.of(
             List.of("r1c1", "r1c2", "r1c3"),
             List.of("r2c1", "r2c2", "r2c3")
         ))),
         entry("comments", new NsvData(
-            List.of("v:1.0", "# This is a comment", "// Another comment", "-- And another"),
+            List.of("# This is a comment", "// Another comment", "-- And another"),
             List.of(List.of("r1c1", "r1c2"), List.of("r2c1", "r2c2"))
         )),
-        entry("empty_fields", new NsvData(List.of("v:1.0"), List.of(
+        entry("empty_fields", new NsvData(List.of(), List.of(
             List.of("r1c1", "", "r1c3"),
             List.of("r2c1", "", "r2c3")
         ))),
-        entry("empty_sequence", new NsvData(List.of("v:1.0"), List.of(
+        entry("empty_sequence", new NsvData(List.of(), List.of(
             List.of("r1c1", "r1c2"), 
             List.of(), 
             List.of("r3c1", "r3c2")
         ))),
-        entry("empty_sequence_end", new NsvData(List.of("v:1.0"), List.of(
+        entry("empty_sequence_end", new NsvData(List.of(), List.of(
             List.of("r1c1", "r1c2"), 
             List.of("r2c1", "r2c2"), 
             List.of()
         ))),
-        entry("empty_sequence_start", new NsvData(List.of("v:1.0"), List.of(
+        entry("empty_sequence_start", new NsvData(List.of(), List.of(
             List.of(), 
             List.of("r2c1", "r2c2"), 
             List.of("r3c1", "r3c2")
         ))),
-        entry("special_chars", new NsvData(List.of("v:1.0"), List.of(
+        entry("special_chars", new NsvData(List.of(), List.of(
             List.of("field with spaces", "field,with,commas", "field\twith\ttabs"),
             List.of("field\"with\"quotes", "field'with'quotes", "field\\with\\backslashes"),
             List.of("field\nwith\nnewlines", "field, just field")
         ))),
-        entry("multiple_empty_sequences", new NsvData(List.of("v:1.0"), List.of(
+        entry("multiple_empty_sequences", new NsvData(List.of(), List.of(
             List.of(),
             List.of("r2c1", "r2c2"),
             List.of(),
@@ -57,7 +57,7 @@ public class TestUtils {
             List.of("r5c1", "r5c2", "r5c3"),
             List.of()
         ))),
-        entry("multiline_encoded", new NsvData(List.of("v:1.0"), List.of(
+        entry("multiline_encoded", new NsvData(List.of(), List.of(
             List.of("line1\nline2", "r1c2", "r1c3"),
             List.of("anotherline1\nline2\nline3", "r2c2")
         )))
